388561eebc1a2228dbc6db10097f3791b427f2dd
André Kaustell <andre.kaustell@controlthings.fi>
2016-10-18 10:15:55 +0300

    Cleanup

--- node_modules/mist-device/node_modules/wish-app/node_modules/wish-rpc/src/index.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    Server: require('./rpc-server.js').Server,
-    Client: require('./rpc-client.js').Client,
-    // Backwards compatible
-    RPC: require('./rpc-server.js').Server
-};
