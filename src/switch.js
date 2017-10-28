2489c351068636f59cc3d2e36f8eee5963b45560
André Kaustell <andre.kaustell@controlthings.fi>
2017-10-28 11:06:03 +0300

    Updated to new use new Mist Api.

--- src/switch.js
+++ src/switch.js
@@ -1,27 +1,28 @@
 var EventEmitter = require('events').EventEmitter;
 var MistNode = require('mist-api').MistNode;
-var model = require('./model.json');
 var util = require("util");
 
 var name = process.env.NAME || 'Switch';
+var state = false;
 
 if (!process.env.NAME) { console.log('Use: NAME="Switch Label" to run several instances.'); }
 
 function Switch(id) {
     var node = new MistNode({ name: name }); // , coreIp: '127.0.0.1', corePort: 9094
         
-    node.create(model);
-    
-    node.update('mist.name', name);
-    
-    node.invoke('config', function(args, cb) {
-        cb({ cool: ['a', 7, true], echo: args });
+    node.endpointAdd('mist', { type: 'string' });
+    node.endpointAdd('mist.name', { type: 'string', read: function(args, peer, cb) { cb(null, name); } });
+    node.endpointAdd('state', { 
+        type: 'bool',
+        read: function(args, peer, cb) { cb(null, state); },
+        write: function(value, peer, cb) {
+            state = !!value;
+            console.log('Node write state:', state);
+            cb();
+            node.changed('state');
+        }
     });
-
-    node.write(function(epid, data) {
-        console.log('Node write:', epid, data);
-        if (epid === 'state') { node.update(epid, !!data); }
-    });
+    node.endpointAdd('config', { type: 'invoke', invoke: function(args, peer, cb) { cb(null, { cool: ['a', 7, true], echo: args }); } });
 }
 
 util.inherits(Switch, EventEmitter);
