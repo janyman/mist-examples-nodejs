e5ee574614dcf49356b7dc095150f02ade7179cc
André Kaustell <andre.kaustell@controlthings.fi>
2018-01-11 17:14:11 +0200

    Updated to work with newest mist-api.

--- src/switch.js
+++ src/switch.js
@@ -1,30 +1,38 @@
-var EventEmitter = require('events').EventEmitter;
 var MistNode = require('mist-api').MistNode;
-var model = require('./model.json');
-var util = require("util");
 
 var name = process.env.NAME || 'Switch';
 
 if (!process.env.NAME) { console.log('Use: NAME="Switch Label" to run several instances.'); }
 
 function Switch(id) {
+    var relay = false;
+    
     var node = new MistNode({ name: name }); // , coreIp: '127.0.0.1', corePort: 9094
-        
-    node.create(model);
-    
-    node.update('mist.name', name);
-    
-    node.invoke('config', function(args, cb) {
-        cb({ cool: ['a', 7, true], echo: args });
-    });
 
-    node.write(function(epid, data) {
-        console.log('Node write:', epid, data);
-        if (epid === 'state') { node.update(epid, !!data); }
+    // add `mist` endpoint
+    node.addEndpoint('mist', { type: 'string' });
+    // add `mist.name` as subendpoint to mist
+    node.addEndpoint('mist.name', { type: 'string', read: function(args, peer, cb) { cb(null, name); } });
+    
+    // add readable and writable `number` endpoint
+    node.addEndpoint('relay', {
+        type: 'bool',
+        read: function(args, peer, cb) {
+            cb(null, relay);
+            
+            if (!peer) { /* local read */ return; }
+            node.request(peer, 'control.invoke', ['debug', 'You read my values!'], console.log);
+        },
+        write: function(value, peer, cb) {
+            // write the internal state variable for `number` endpoint
+            relay = !!value;
+            // signal successful write
+            cb();
+            // signal `number` value changed
+            node.changed('relay');
+        }
     });
 }
-
-util.inherits(Switch, EventEmitter);
 
 module.exports = {
     Switch: Switch
